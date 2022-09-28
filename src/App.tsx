e311ef72dc363ed5ff01a2d03ed918cbb6c6986f
dankelleher <dankelleher@yahoo.com>
2022-09-28 10:43:46 +0200

    Support reflect-metadata in react

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ import {Field, PrivateKey, Proof, UInt32} from "snarkyjs";
 const App = () => {
   const [building, setBuilding] = useState<boolean>(false);
   const [prover, setProver] = useState<Prover>();
+  const [error, setError] = useState<string>();
   const [proof, setProof] = useState<Proof<Field>>();
+  const [age, setAge] = useState<number>(36);
 
   useEffect(() => {
     console.log("Building prover...", building);
@@ -18,6 +20,7 @@ const App = () => {
         setBuilding(false);
       }).catch((e) => {
         console.log("Building failed")
+        setError(e.toString());
         console.error(e);
       });
     }
@@ -26,28 +29,25 @@ const App = () => {
   const prove = useCallback(async () => {
     if (prover) {
       console.log("Proving...")
-      const user = new User(PrivateKey.random().toPublicKey(), UInt32.from(36));
+      const user = new User(PrivateKey.random().toPublicKey(), UInt32.from(age));
       const result = await prover.generateProof(user);
       setProof(result);
       console.log("proof: ", result);
     }
-  }, [prover]);
+  }, [prover, age]);
 
   return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <p>
-            <button onClick={prove} disabled={!prover}>Prove</button>
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} width="200vw" className="App-logo" alt="logo"/>
+          <div>
+            <div style={{ marginBottom: "2px" }}>
+              <label htmlFor="age">Age: </label>
+              <input id="age" onChange={(e) => setAge(parseInt(e.target.value))} type="number" value={age} />
+              <button onClick={prove} disabled={!prover}>Prove</button>
+            </div>
+            <textarea style={{ width: "20vw"}} value={proof?.toString() || error || ""} readOnly={true}/>
+          </div>
         </header>
       </div>
   );
